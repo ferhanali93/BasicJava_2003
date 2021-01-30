f97e29d031dc84bbc49d11c1fe365f8e6183d99a
ferhanali <Bronx_10460>
2021-01-30 17:43:29 -0500

    Added few more classes and did some modification

---
+++ src/Batch2003.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Batch2003 {
+    public static void main(String[] args) {
+        String name;
+        int idNum;
+        char grade;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter student's Id Number: ");
+        idNum = sc.nextInt();
+
+        System.out.println("Enter student's Name: ");
+        name = sc.next();
+
+        System.out.println("you entered id# " + idNum + " and Name: " + name);
+        grade = 'A';
+
+        System.out.println("you grade is: " + grade);
+    }
+}
---
+++ src/Grades.java
@@ -0,0 +1,25 @@
+public class Grades {
+    public static void main(String[] args) {
+        findGrades("Ferhan", 90);
+        findGrades("Monir", 95);
+        findGrades("Huma", 88);
+        findGrades("Sadika", 75);
+
+    }
+    public static void findGrades(String studentName, int score){
+        String grade = null;
+        if (score >= 90 &&score <= 100){
+            grade = "A";
+        } else if (score >= 80 && score < 90){
+            grade = "B";
+        } else if (score >= 70 && score < 80){
+            grade = "C";
+        } else if (score >= 60 && score < 70){
+            grade = "D";
+        } else if (score < 60){
+            grade = "F";
+        }
+        
+        System.out.println("Grade of " + studentName + " is " + grade);
+    }
+}
